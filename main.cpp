6e89d37ebcbae07f83df31c7896341d81fef1b9e
Ignatiy Glushkov <64589798+toniess@users.noreply.github.com>
2024-01-29 00:16:19 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <vector>
+#include <map>
+
+// Реализуйте класс SqlSelectQueryBuilder, который можно использовать для построения простых SELECT-запросов.
+
+// Пример использования:
+// SqlSelectQueryBuilder builder;
+// builder.select({"id", "name", "age"});
+// builder.from("students");
+// builder.where("age > 18");
+
+class SqlSelectQueryBuilder
+{
+
+public:
+    explicit SqlSelectQueryBuilder() = default;
+
+
+    std::string buildQuery() {
+        if (from.length() == 0)
+            return "*";
+        return "SELECT " + column + " FROM " + from + " WHERE " + where + ";";
+    }
+
+
+    SqlSelectQueryBuilder& addColumn(const std::string& column) {
+        if (this->column.length() > 0)
+            this->column += ", ";
+        this->column += column;
+        return *this;
+    }
+
+
+    SqlSelectQueryBuilder& addColumns(const std::vector<std::string>& columns) {
+
+        for (auto column : columns) {
+            if (this->column.length() > 0)
+                this->column += ", ";
+            this->column += column;
+        }
+        return *this;
+    }
+
+
+    SqlSelectQueryBuilder& addWhere(const std::string& column, const std::string& value) {
+        if (this->where.length() > 0)
+            this->where += " AND ";
+        this->where += column + "=" + value;
+        return *this;
+    }
+
+
+    SqlSelectQueryBuilder& addWhere(const std::map<std::string, std::string>& kv) {
+
+        for (auto& [column, value] : kv) {
+            if (this->where.length() > 0)
+                this->where += " AND ";
+            this->where += column + "=" + value;
+        }
+        return *this;
+    }
+
+
+    SqlSelectQueryBuilder& addFrom(std::string from) {
+        this->from = from;
+        return *this;
+    }
+
+
+private:
+    std::string from = "";
+    std::string where = "";
+    std::string column = "";
+};
+
+int main()
+{
+
+    // Задание 1 (нерасширенный класс)
+    {
+        SqlSelectQueryBuilder query_builder;
+        query_builder.addFrom("students");
+        query_builder.addColumn("name").addColumn("phone");
+        query_builder.addWhere("id", "42").addWhere("name", "John");
+
+        assert(query_builder.buildQuery() == "SELECT name, phone FROM students WHERE id=42 AND name=John;"
+               && "QUERY BUILDER TEST FAILED" );
+        std::cout << "Test 1 passed" << std::endl;
+    }
+
+    // Задание 2 (расширенный класс)
+    {
+        SqlSelectQueryBuilder query_builder;
+        query_builder.addFrom("students");
+        query_builder.addColumns({"name", "phone"});
+        query_builder.addWhere( { {"id", "42"}, {"name", "John"} } );
+
+        assert(query_builder.buildQuery() == "SELECT name, phone FROM students WHERE id=42 AND name=John;"
+               && "QUERY BUILDER TEST FAILED" );
+        std::cout << "Test 2 passed" << std::endl;
+    }
+
+    return 0;
+}
